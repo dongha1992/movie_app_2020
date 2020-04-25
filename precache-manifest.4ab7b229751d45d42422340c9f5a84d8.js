a73e5fde84844f8fb42319eb9f78387061819075
donghakim <kamdunga2002@gmail.com>
2020-04-25 22:23:13 +0900

    Updates

---
+++ precache-manifest.4ab7b229751d45d42422340c9f5a84d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc146254766c6a409229fc1794a4778",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "794d8cdd37a81caba001",
+    "url": "/movie_app_2020/static/css/main.a09d0269.chunk.css"
+  },
+  {
+    "revision": "67f474c31357c2946070",
+    "url": "/movie_app_2020/static/js/2.ebfe2a52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.ebfe2a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794d8cdd37a81caba001",
+    "url": "/movie_app_2020/static/js/main.77fce3f2.chunk.js"
+  },
+  {
+    "revision": "e76fc168c1d893da3cab",
+    "url": "/movie_app_2020/static/js/runtime-main.ea46b0e5.js"
+  }
+]);
